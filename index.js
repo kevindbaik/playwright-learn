cd32e2dc20cecfa29666b993ed42570b0b13de49
kevin <kevindbaik@gmail.com>
2024-05-16 18:12:43 -0700

    implemented article scraping, added comments for better clarity

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ import inquirer from 'inquirer';
 async function fetchHackerNewsData(options) {
   // Placeholder for your fetchHackerNewsData function implementation
   console.log("Fetching Hacker News data with options:", options);
+
+  // create browser
+  const browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  // go to desired page (either today, newest, or past)
+  const url = await goToPage(options.url);
+  await page.goto(url);
+
+  // save the articles
+  const articles = await getArticles(page, options.articles, options.sort);
+  console.log('articles', articles)
 }
 
 
@@ -27,7 +40,7 @@ async function run() {
   ]);
 
   if (fetchType === 'normal') {
-    await fetchHackerNewsData({page: 'home', articles: 10, sort: 'none'});
+    await fetchHackerNewsData({url: 'today', articles: 10, sort: 'none'});
   } else if (fetchType === 'custom') {
     const customQuery = await inquirer.prompt([
       {
@@ -76,9 +89,87 @@ async function run() {
 
     // customQuery object created and looks like:
       // { page : "newest", articles: 30, sort: 'upvotes' }
-    await fetchHackerNewsData({page: customQuery.page, articles: customQuery.articles, sort: customQuery.sort})
+    await fetchHackerNewsData({url: customQuery.page, articles: customQuery.articles, sort: customQuery.sort})
   }
 }
+
+// helper functions
+// purpose: generate url link
+  // parameter: string, returns: string
+async function goToPage(url) {
+  const options = {
+    today: 'https://news.ycombinator.com',
+    newest: 'https://news.ycombinator.com/newest',
+    past: 'https://news.ycombinator.com/front'
+  };
+  return options[url]
+}
+
+// purpose:
+async function getArticles(page, articleCount, sortType) {
+  let articles = [];
+
+  // give small time to allow data to load onto screen before i do anything
+  await page.waitForTimeout(1000);
+
+  // stop when i have the # of articles requested
+  while (articles.length < articleCount) {
+  const article = await page.evaluate(() => {
+    const results = [];
+    const articles = document.querySelectorAll('.athing');
+
+    articles.forEach(element => {
+      const titleElement = element.querySelector('.titleline > a');
+      const title = titleElement ? titleElement.innerText : 'No title';
+      const url = titleElement ? titleElement.href : 'Unknown link';
+
+      const subtextElement = element.nextElementSibling.querySelector('.subtext');
+      const authorElement = subtextElement.querySelector('.hnuser');
+      const author = authorElement ? authorElement.innerText : 'Unknown Author';
+
+      const timeElement = subtextElement.querySelector('.age');
+      const time = timeElement ? timeElement.innerText : 'Unknown time';
+
+      const scoreElement = subtextElement.querySelector('.score');
+      const scoreText = scoreElement ? scoreElement.innerText : '0 points';
+      const upvotes = parseInt(scoreText.split(' ')[0]) || 0;
+
+      const commentElements = subtextElement.querySelectorAll('a');
+      // HTML structure for Hacker News has a lot of a tags, here i just get the LAST one
+      const numCommentsText = commentElements.length > 0 ? commentElements[commentElements.length - 1].innerText : '0 comments';
+      const numComments = parseInt(numCommentsText.split(' ')[0]) || 0;
+
+      results.push({ title, url, author, time, upvotes, comments: numComments });
+    });
+    return results;
+  })
+
+  // add new articles (30) to my main array
+  articles.push(...article)
+
+  // if we have collected enough articles, leave.. otherwise go to next page
+  if (articles.length >= articleCount) break;
+    const nextPage = await page.$('a.morelink');
+    if (nextPage) {
+      await nextPage.click();
+      await page.waitForNavigation();
+    } else {
+      break;
+    }
+  }
+  // loop is finished now, we have our articles array
+  // now sort
+    if (sortType === 'upvotes') {
+      articles.sort((a, b) => b.upvotes - a.upvotes);
+    } else if (sortType === 'comments') {
+      articles.sort((a, b) => b.comments - a.comments);
+    }
+
+   // return the top articles based on the articleCount
+   return articles.slice(0, articleCount);
+}
+
+
 
 (async () => {
   await run()
